e10545fd942cc69a2bd726addb51b5f32347e09c
tcellcode <99007786+tcellcode@users.noreply.github.com>
2026-09-14 12:39:20 +0700

    Update dsu-class.cpp

--- dsu-class.cpp
+++ dsu-class.cpp
@@ -1,52 +1,36 @@
-#include <vector>
-#include <algorithm> // For std::swap
-
-class DSU {
-private:
-    std::vector<int> parent;
-    std::vector<int> size; // Added to track the size of each set
-
-public:
-    // Initialize the DSU with 'n' elements
-    DSU(int n) {
-        parent.resize(n);
-        size.assign(n, 1); // Every set initially has a size of 1
-        for (int i = 0; i < n; i++) {
-            parent[i] = i; // Each element is initially its own root
-        }
-    }
-
-    // Find operation with Path Compression
-    int find(int u) {
-        if (parent[u] == u) {
-            return u;
-        }
-        // Compress the path by pointing directly to the root
-        return parent[u] = find(parent[u]);
-    }
-
-    // Union operation with Union by Size
-    // Returns true if a merge happened, false if they were already connected
-    bool join(int u, int v) {
-        int rootU = find(u);
-        int rootV = find(v);
-
-        // If they have the same root, they are already in the same set
-        if (rootU == rootV) {
-            return false;
+class DSU
+{
+    private: 
+        vector<int> par;
+        vector<int> sz;
+    public:
+        DSU(int n)
+        {
+            par.resize(n + 5); sz.resize(n + 5, 1);
+            for (int i = 1; i <= n; i++) par[i] = i;
+            
         }
 
-        // Ensure rootU represents the larger set
-        if (size[rootU] < size[rootV]) {
-            std::swap(rootU, rootV);
+        int acs(int u)
+        {
+            if (par[u] == u) return u;
+            return par[u] = acs(par[u]);
         }
 
-        // Merge the smaller set (rootV) into the larger set (rootU)
-        parent[rootV] = rootU;
-        
-        // Update the size of the new combined set
-        size[rootU] += size[rootV];
-        
-        return true; 
-    }
+        void join(int u, int v)
+        {
+            int ru = acs(u), rv = acs(v);
+            if (ru == rv) return; // same set
+            
+            // root u has the largest size
+            if (sz[ru] < sz[rv]) swap(ru, rv);
+            par[rv] = ru;
+            sz[ru] += sz[rv];
+        }
+
+        bool connected(int u, int v)
+        {
+            int ru = acs(u), rv = acs(v);
+            return (ru == rv);
+        }
 };
