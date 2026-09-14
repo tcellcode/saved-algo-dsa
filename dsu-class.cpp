7602422b82234e83a27938952b87f11a55d38dc3
tcellcode <99007786+tcellcode@users.noreply.github.com>
2026-09-14 12:29:38 +0700

    Update dsu-class.cpp

--- dsu-class.cpp
+++ dsu-class.cpp
@@ -1,13 +1,16 @@
 #include <vector>
+#include <algorithm> // For std::swap
 
 class DSU {
 private:
     std::vector<int> parent;
+    std::vector<int> size; // Added to track the size of each set
 
 public:
     // Initialize the DSU with 'n' elements
     DSU(int n) {
         parent.resize(n);
+        size.assign(n, 1); // Every set initially has a size of 1
         for (int i = 0; i < n; i++) {
             parent[i] = i; // Each element is initially its own root
         }
@@ -22,7 +25,7 @@ public:
         return parent[u] = find(parent[u]);
     }
 
-    // Union operation
+    // Union operation with Union by Size
     // Returns true if a merge happened, false if they were already connected
     bool join(int u, int v) {
         int rootU = find(u);
@@ -33,8 +36,17 @@ public:
             return false;
         }
 
-        // Merge the sets
-        parent[rootU] = rootV;
+        // Ensure rootU represents the larger set
+        if (size[rootU] < size[rootV]) {
+            std::swap(rootU, rootV);
+        }
+
+        // Merge the smaller set (rootV) into the larger set (rootU)
+        parent[rootV] = rootU;
+        
+        // Update the size of the new combined set
+        size[rootU] += size[rootV];
+        
         return true; 
     }
 };
