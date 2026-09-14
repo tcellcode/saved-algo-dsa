f5f357aba522a98d0dde93d88e80874b881a1e2b
tcellcode <99007786+tcellcode@users.noreply.github.com>
2026-09-14 12:10:03 +0700

    Create dsu-plain.cpp

---
+++ dsu-plain.cpp
@@ -0,0 +1,31 @@
+// 1. Path Compression
+int acs(int u) 
+{
+    if (par[u] == u) 
+        return u;
+    
+    // Update the parent directly during the return step
+    return par[u] = acs(par[u]); 
+}
+
+// 2. Union by Size
+void join(int u, int v) 
+{
+    int x = acs(u);
+    int y = acs(v);
+    
+    if (x != y) 
+    {
+        // Ensure x always represents the larger set
+        if (sz[x] < sz[y]) 
+        {
+            swap(x, y); 
+        }
+        
+        // Attach the smaller tree (y) to the larger tree (x)
+        par[y] = x;     
+        
+        // Update the size of the new combined set
+        sz[x] += sz[y]; 
+    }
+}
